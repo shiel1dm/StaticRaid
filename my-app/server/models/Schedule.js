04d417a14a88961cc507b9c9ad6117dcbce25690
depotte6 <depotte6@gmail.com>
2021-10-28 17:39:54 -0400

    updated User, Team, Schedule schemas and models

--- my-app/server/models/Schedule.js
+++ my-app/server/models/Schedule.js
@@ -3,5 +3,12 @@ const mongoose = require('mongoose');
 const { Schema } = mongoose;
 
 const scheduleSchema = new Schema({
+    gameTime: {
+        type: String,
+        required: 'Please pick a time!',
+        trim: true,
+    },
+});
 
-})
+
+module.exports = scheduleSchema;
