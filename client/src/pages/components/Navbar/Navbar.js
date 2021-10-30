ba11c1c81ef9359084fe67bfa69aff194a567867
depotte6 <depotte6@gmail.com>
2021-10-30 17:39:29 -0400

    Still working on visibility of componenets with materialUI

--- client/src/pages/components/Navbar/Navbar.js
+++ client/src/pages/components/Navbar/Navbar.js
@@ -2,12 +2,25 @@ import React, { useState, useEffect } from 'react';
 import { Link } from 'react-router-dom';
 import decode from 'jwt-decode';
 import { styles } from './styles.js';
-import { AppBar, Button, Typography, Toolbar, Avatar } from '@material-ui/core';
-
+import { AppBar, Button, Typography, Toolbar, Avatar, IconButton, Box, FormGroup, FormControlLabel, Menu, MenuItem,  Switch } from '@material-ui/core';
+import { AccountCircle} from '@material-ui/icons';
 
 const Navbar = () => {
-    
+    const [auth, setAuth] = React.useState(true);
+    const [anchorEl, setAnchorEl] = React.useState(null);
    const [user, setUser] = useState(JSON.parse(localStorage.getItem('profile')));
+   const handleChange = (event) => {
+    setAuth(event.target.checked);
+  };
+
+  const handleMenu = (event) => {
+    setAnchorEl(event.currentTarget);
+  };
+
+  const handleClose = () => {
+    setAnchorEl(null);
+  };
+
     
     useEffect(() => {
         
@@ -17,8 +30,11 @@ const Navbar = () => {
             history.push('/auth');
         
             setUser(null);
-          };
+        };
         
+            
+          
+           
         const token = user?.token;
     
         if (token) {
@@ -26,16 +42,77 @@ const Navbar = () => {
     
           if (decodedToken.exp * 1000 < new Date().getTime()) logout();
         }
+
         setUser(JSON.parse(localStorage.getItem('profile')));
-  }, [location]);
+        }, [location]);
+        
 
 
 
     return (
+        <Box sx={{ flexGrow: 1 }}>
+        <FormGroup>
+          <FormControlLabel
+            control={
+              <Switch
+                checked={auth}
+                onChange={handleChange}
+                aria-label="login switch"
+              />
+            }
+            label={auth ? 'Logout' : 'Login'}
+          />
+        </FormGroup>
         <AppBar>
         <div className = "Navbar" position = "static" color="default">
             <h2>StaticRaid</h2>
         </div>
+          <Toolbar>
+            <IconButton
+              size="large"
+              edge="start"
+              color="inherit"
+              aria-label="menu"
+              sx={{ mr: 2 }}
+            >
+                </IconButton>
+  
+        {auth && (
+            <div>
+              <IconButton
+                size="large"
+                aria-label="account of current user"
+                aria-controls="menu-appbar"
+                aria-haspopup="true"
+                onClick={handleMenu}
+                color="inherit"
+              >
+                <AccountCircle />
+              </IconButton>
+              <Menu
+                id="menu-appbar"
+                anchorEl={anchorEl}
+                anchorOrigin={{
+                  vertical: 'top',
+                  horizontal: 'right',
+                }}
+                keepMounted
+                transformOrigin={{
+                  vertical: 'top',
+                  horizontal: 'right',
+                }}
+                open={Boolean(anchorEl)}
+                onClose={handleClose}
+              >
+                <MenuItem onClick={handleClose}>Profile</MenuItem>
+                <MenuItem onClick={handleClose}>My account</MenuItem>
+
+              </Menu>
+              </div>
+        )
+            }</Toolbar>
+              </AppBar>
+              
         <div className = "container">
             <Typography component={Link} to="/" className="heading"></Typography>
             </div>
@@ -54,11 +131,15 @@ const Navbar = () => {
                     </Button>
                     
                 )}
-                
             </div>
-        </AppBar>
+        
+               
+    
+    
+        </Box>
     )
 }
+
 
 
 
