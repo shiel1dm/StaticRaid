991be0cb10831665888516fff6a721efbbdeef0c
Cialysse <marshall.cierra714@gmail.com>
2021-11-04 11:13:41 -0400

    merge with main

--- client/src/components/About.js
+++ client/src/components/About.js
@@ -1,5 +1,6 @@
 import React from 'react'
 import { makeStyles } from '@material-ui/core/styles';
+import { Typography } from '@material-ui/core';
 //import TeamCard from './TeamCard';
 
 const useStyles = makeStyles((theme) => ({
@@ -12,7 +13,9 @@ export default function About() {
     const classes = useStyles();
     return (
         <div className={classes.root}>
-
+            <Typography>
+                Welcome!
+            </Typography>
 
         </div>
     )
