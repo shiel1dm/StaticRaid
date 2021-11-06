28ea271f9cb9e37fe621cb5c00d1233a4b9e03b7
Cialysse <marshall.cierra714@gmail.com>
2021-11-05 22:34:43 -0400

    started implementing the schedule api

--- client/src/pages/Scheduler.js
+++ client/src/pages/Scheduler.js
@@ -1,40 +1,18 @@
 import React from 'react'
-import 'date-fns'
-import Grid from '@material-ui/core/Grid'
-import DateFnsUtils from '@date-io/date-fns'
-import { MuiPickersUtilsProvider, KeyboardTimePicker, KeyboardDatePicker } from '@material-ui/pickers'
+import Nylas from 'nylas';
 
-function Scheduler() {
+Nylas.config((
+    clientId : 'aq4yv5s2t1b56q5y2vn3dph9a',
+    clientSecret: '37t2flhoxx4uo5b6qsw581v7',
+));
 
-    const [selectedDate, setSelectedDate] = React.useState(
-        new Date("2021-11-4T12:00:00")
+const nylas = Nylas.with(UgFNbppUjTuzNArfDtB5THVeIJpIvU);
+nylas.events
+.count()
+.then(count => {
+    console.log(
+        
     )
-
-    const handleDateChange = (date) => {
-        setSelectedDate(date)
-    }
-
-    return (
-        <div className='date'>
-            <MuiPickersUtilsProvider utils={DateFnsUtils}>
-            <Grid container justify='space-around'>
-                <KeyboardDatePicker 
-                disableToolbar
-                variant='inline'
-                format='MM/dd/yyyy'
-                margin='normal'
-                id='date-picker'
-                label='Date Pickervalue'
-                value={selectedDate}
-                onChange={handleDateChange}
-                KeyboardButtonProps={{
-                    'aria-label': 'change date'
-                }}
-                />
-            </Grid>
-        </MuiPickersUtilsProvider>
-        </div>
-    )
-}
+})
 
 export default Scheduler
