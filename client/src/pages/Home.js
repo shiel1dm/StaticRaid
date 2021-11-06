1ed7a29cc6892df50bedc853553a4938c33d8f0b
Cialysse <marshall.cierra714@gmail.com>
2021-11-05 21:39:31 -0400

    merging with main

--- client/src/pages/Home.js
+++ client/src/pages/Home.js
@@ -1,45 +1,21 @@
-import React, { useEffect, useState, Component } from 'react'
-import { createTheme, ThemeProvider } from '@mui/material/styles';
-import { makeStyles } from '@material-ui/core/styles';
-import { CssBaseline } from '@material-ui/core';
-import Header from '../components/Header'
-import About from '../components/About';
-import ScheduleCard from '../components/ScheduleCard';
+// import React, { useEffect, useState, Component } from 'react'
+// import { createTheme, ThemeProvider } from '@mui/material/styles';
+// import { makeStyles } from '@material-ui/core/styles';
+// import { CssBaseline } from '@material-ui/core';
+// import About from './About';
+// import ScheduleCard from '../components/ScheduleCard';
 
 
 
-const useStyles = makeStyles((theme) => ({
-    root: {
-        display: 'flex',
-        justifyContent: 'center',
-        textAlign: 'center',
-        alighnItems: 'center',
-        height: '100vh',
-        backgroundColor: '#54648c',
-    },
-}));
+// const useStyles = makeStyles((theme) => ({
+//     root: {
+//         display: 'flex',
+//         justifyContent: 'center',
+//         textAlign: 'center',
+//         alighnItems: 'center',
+//         height: '100vh',
+//         backgroundColor: '#54648c',
+//     },
+// }));
 
 
-export default function Home() {
-const classes = useStyles();
-    
-return (
-    <div className={classes.root}>
-         <Box className={classes.typedContainer}>
-            <Typography className={classes.title} variant="h4">
-                <Typed strings={["Welcome to Static Raid!"]} typeSpeed={40}/>
-            </Typography>
-        <br />
-        <Typography className={classes.subtitle} variant="h4">
-                <Typed strings={["Game Scheduler", "Team Builder"]} typeSpeed={40}
-                backSpeed={60}
-                loop />
-            </Typography>
-    </Box>
-        <CssBaseline />
-        <Header />
-        <About />
-        <ScheduleCard />
-    </div>
-    )
-};
